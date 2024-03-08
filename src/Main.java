cd10441563a2f0d8c47645089e31a6f88f0f604e
Konstantin Usichenko <usichenkokonstantinvladimirov@gmail.com>
2024-03-07 23:50:00 -0800

    Added code to check if the path is a file or a directory. Additionally it prints count of true entered pathes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,35 @@
+import java.io.File;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>");
 
-        String text = new Scanner(System.in).nextLine();
+        int countPathTrue = 0;
+        while (true) {
 
-        System.out.println("Длина текста: " + text.length());
+            System.out.println("Укажите путь");
+            String path = new Scanner(System.in).nextLine();
+            System.out.println("Указанный Вами путь: " + path);
 
+            File file = new File(path);
+            boolean fileExists = file.exists();
+
+            if (!fileExists) {
+                System.out.println("Файл не существует");
+                continue;
+            }
+
+            boolean isDirectory = file.isDirectory();
+
+            if (isDirectory) {
+                System.out.println("Путь является директорией");
+            } else {
+                countPathTrue++;
+                System.out.println("Путь указан верно");
+
+                System.out.println("Это файл номер: " + countPathTrue);
+            }
+
+        }
     }
 }
