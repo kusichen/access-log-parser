7f1a78be3335a67bc3bc4968878928ff6f5eca9c
Konstantin Usichenko <usichenkokonstantinvladimirov@gmail.com>
2024-04-27 00:50:32 -0700

    code was refactored and added new logic that count requests from YandexBot and  Googlebot

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         int countPathTrue = 0;
         int allLength = 0;
+        int googleRequestCount = 0;
+        int yandexRequestCount = 0;
         while (true) {
 
             System.out.println("Укажите путь");
@@ -37,28 +38,51 @@ public class Main {
                 FileReader fileReader = new FileReader(path);
                 BufferedReader reader =
                         new BufferedReader(fileReader);
-                String line1;
+                String line;
                 int max = 0;
-                while ((line1 = reader.readLine()) != null) {
-                    max = line1.length();
+
+                while ((line = reader.readLine()) != null) {
+                    max = line.length();
                     allLength++;
                     if (max > 1024) throw new InvalidLengthException("Найдена строка длиннее " + max + " символов");
                 }
-                System.out.println("Количество строк " + allLength);
-                System.out.println("Самая длинная строка имеет " + max + " символов");
-                List<String> lines = Files.readAllLines(Paths.get(path));
-                int min = Integer.MAX_VALUE;
-                String minLenght = "";
-                for (String line2 : lines) {
-                    int length = line2.length();
+                String filePath = path;
+                List<String> lines = Files.readAllLines(Paths.get(filePath));
+                String[] linesArray = lines.toArray(new String[lines.size()]);
 
-                    if (length < min) {
-                        min = length;
-                        minLenght = line2;
+                for (String newlines : linesArray) {
+                    int first = newlines.indexOf('(');
+                    int second = newlines.indexOf(')') - 1;
+                    if (first < second) {
+                        String substring = newlines.substring(first, second);
+                        String[] parts = substring.split(";");
+                        if (parts.length >= 2) {
+                            String fragment = parts[1];
+                            String[] fragments = fragment.split(" ");
+
+                            for (int i = 0; i < fragments.length; i++) {
+                                fragments[i] = fragments[i].trim();
+                            }
+                            String fragment2 = Arrays.toString(fragments);
+                            if (fragments.length > 0) {
+                                int slash = fragment2.indexOf('/');
+                                if (slash > 0) {
+                                    String beforeSlash = fragment2.substring(2, slash);
+                                    if (beforeSlash.contains("Googlebot")) {
+                                        googleRequestCount++;
+                                    } else if (beforeSlash.contains("YandexBot")) {
+                                        yandexRequestCount++;
+                                    }
+                                }
+                            }
+
+                        }
                     }
                 }
-                System.out.println("Самая короткая строка " + minLenght);
-                System.out.println("Самая короткая строка имеет " + minLenght.length() + " символов");
+
+                System.out.println("Количество строк " + allLength);
+                System.out.println("Количество запросов от Googlebot: " + googleRequestCount);
+                System.out.println("Количество запросов от YandexBot: " + yandexRequestCount);
 
             } catch (InvalidLengthException ex) {
                 System.err.println(ex.getMessage());
@@ -69,6 +93,7 @@ public class Main {
             }
         }
     }
+
     static class InvalidLengthException extends RuntimeException {
         public InvalidLengthException(String message) {
             super(message);
